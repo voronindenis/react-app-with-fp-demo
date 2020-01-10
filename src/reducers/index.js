65a6e4b4508a1e6ad2c9de4b1e0308dfb548ca44
Denis Voronin <mystimind@mail.ru>
2020-01-10 18:29:22 +0300

    changed TAction generics

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -6,6 +6,6 @@ type TRootReducer = {
   'ARTIST_CONTENT_SEARCH': Function,
 };
 
-export const rootReducer = combineReducers<TRootReducer, TAction<Object, Object>>({
+export const rootReducer = combineReducers<TRootReducer, TAction<any, any>>({
   [ARTIST_CONTENT_SEARCH_STORE_KEY]: artistContentSearchReducer,
 });
